9fafb92f2a8aa8d726f4c6ee333fde0b4e2e6d4c
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2015-07-11 18:07:58 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ HERE = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(HERE, 'README.rst')).read()
 CHANGELOG = open(os.path.join(HERE, 'CHANGELOG.rst')).read()
 
-VERSION = '0.0.5'
+VERSION = '0.0.6'
 
 setup(name='pyagar',
       version=VERSION,
